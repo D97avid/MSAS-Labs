81f2ad5efd152e389ba8fac3538c39b22f670c3f
D97avid <90031117+D97avid@users.noreply.github.com>
2021-11-22 10:56:13 +0100

    ver3.0.0

--- ESERCIZI/Assign2.m
+++ ESERCIZI/Assign2.m
@@ -26,10 +26,12 @@ fclose(fid);
 fun = @(KB) max(norm(acceleration(x0,Data,KB(1),KB(2)) - samples(:,2:3)));  % Function to minimize (maximum absolute error)
 
 KB0 = [k_guess;b_guess];                                                    % Initial guess vector for fmincon
-[KB] = fmincon(fun,KB0,[],[]);
+options = optimoptions("fminunc","Display","off");
+[KB] = fminunc(fun,KB0,options);
 
 k_opt = KB(1);
 b_opt = KB(2);
+
 fprintf("k_opt = %.4f\n",k_opt); fprintf("b_opt = %.4f\n",b_opt);
 
 % Find system response for guessed k_opt and b_opt:
@@ -38,31 +40,31 @@ eps = abs(acc-samples(:,2:3));                                              % Ab
 
 % Plots:
 figure()
-plot(t_guess,acc_guess,"LineWidth",2)
+plot(t_guess,acc_guess(:,1),"LineWidth",2,"Color",[0.3010 0.7450 0.9330])
+hold on
+plot(t_guess,acc_guess(:,2),"LineWidth",2,"Color","#FFE42B")
 grid on; legend('$\dot\theta_1$','$\dot\theta_2$','Interpreter','latex')
 title('System response for $k_{guess}$ and $b_{guess}$','Interpreter','latex'); 
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$\dot\theta \,\, [rad/s^2]$','Interpreter','latex');
 
 figure()
 subplot(1,2,1)
-plot(samples(:,1),samples(:,2),"LineWidth",2)
+plot(samples(2:end,1),samples(2:end,2),"LineWidth",2,"LineStyle","-","Color",[0.3010 0.7450 0.9330])
 hold on
-plot(samples(:,1),samples(:,3),"LineWidth",2)
-grid on; legend('$\dot\theta_1$','$\dot\theta_2$','Interpreter','latex')
-title('Samples','Interpreter','latex');
-xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$\dot\theta \,\, [rad/s^2]$','Interpreter','latex');
-subplot(1,2,2)
-plot(t,acc(:,1),"LineWidth",2)
+plot(t(2:end),acc(2:end,1),"LineWidth",2,"LineStyle",":","Color",[0 0.4470 0.7410])
 hold on
-plot(t,acc(:,2),"LineWidth",2)
-grid on; legend('$\dot\theta_1$','$\dot\theta_2$','Interpreter','latex')
-title('System response for $k_{opt}$ and $b_{opt}$','Interpreter','latex'); xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$\dot\theta \,\, [rad/s^2]$','Interpreter','latex');
+plot(samples(2:end,1),samples(2:end,3),"LineWidth",2,"LineStyle","-","Color","#FFE42B")
+hold on
+plot(t(2:end),acc(2:end,2),"LineWidth",2,"LineStyle",":","Color",[0.9290 0.6940 0.1250])
+grid on; 
+legend('$\dot\theta_{1,sampled}$','$\dot\theta_1$','$\dot\theta_{2,sampled}$','$\dot\theta_2$','Interpreter','latex')
+title('Accelerations','Interpreter','latex');
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$\dot\theta \,\, [rad/s^2]$','Interpreter','latex');
 
-figure()
-semilogy(t,eps(:,1),"LineWidth",2)
+subplot(1,2,2)
+semilogy(t(2:end),eps(2:end,1),"LineWidth",2,"Color",[0.3010 0.7450 0.9330])
 hold on
-semilogy(t,eps(:,2),"LineWidth",2)
+semilogy(t(2:end),eps(2:end,2),"LineWidth",2,"Color","#FFE42B")
 grid on; legend('$\epsilon_1$','$\epsilon_2$','Interpreter','latex')
 title('Absolute error','Interpreter','latex');
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$\epsilon \,\, [-]$','Interpreter','latex');
@@ -119,7 +121,7 @@ data.tF = 3;                                              %[s] Simulation end
 
 % Integration until event (piston position = maximum stroke)
 % State vector: x = [x, v, V_accumulator, V_tank]
-tspan = linspace(data.t0, data.tF,1e3)';                  % Time vector
+tspan = linspace(data.t0, data.tF,1e3)';                 % Time vector
 x0 = [0; 0; data.fluid.V0; data.tank.V0];                 % Initial conditions
 options_BE = odeset('event', @press_event); 
 [t_BE,x_BE,t_event,x_event,~] = ode23s(@pressurization_BE, tspan, x0, options_BE,data); 
@@ -131,98 +133,107 @@ fprintf("t_event = %f\n",t_event)
 
 % After the event, the parameter don't change except the piston velocity
 % which goes to zero:
-
-x = ones(length(tspan),4);
-x(1:i,:) = x_BE;
+t = ones(2*length(t_BE),1);
+t(1:length(t_BE),1) = t_BE;
+t(length(t_BE)+1:end,1) = linspace(t_BE(end),data.tF,length(t)-length(t_BE));
+x = ones(length(t),4);
+x(1:length(t_BE),:) = x_BE;
 x_PE = [1 0 1 1] .* x_event;
-x(i+1:end,:) = x(i+1:end,:) .*x_PE;
+x(length(t_BE)+1:end,:) = x(length(t_BE)+1:end,:) .*x_PE;
 
-param = ones(length(tspan),width(param_BE));
-param(1:i,:) = param_BE;
-param(i+1:end,:) = param(i+1:end,:).*param_BE(end,:);
+param = ones(length(t),width(param_BE));
+param(1:length(t_BE),:) = param_BE;
+param(length(t_BE)+1:end,:) = param(length(t_BE)+1:end,:).*param_BE(end,:);
 
 % Plots:
 figure()
 subplot(2,2,1)
-plot(tspan,x(:,1),'LineWidth',2)
+plot(t,x(:,1),'LineWidth',2)
 hold on
-line(xlim, [x_event(1),x_event(1)], 'Color', 'k', 'LineWidth', 0.5,'LineStyle','--');
-line([t_event,t_event], ylim, 'Color', 'k', 'LineWidth', 0.5, 'LineStyle','--');
 line(xlim, [0,0], 'Color', 'k', 'LineWidth', 0.5);
-plot(tspan,x(:,1),'LineWidth',2,'Color',[0, 0.4470, 0.7410])
+plot(t,x(:,1),'LineWidth',2,'Color',[0, 0.4470, 0.7410])
 hold on
-plot(t_event,x_event(1),'MarkerSize',10,'Marker','pentagram','MarkerFaceColor','m','MarkerEdgeColor','k')
+plot(t_event,x_event(1),'MarkerSize',10,'Marker','pentagram','MarkerFaceColor','m','MarkerEdgeColor','k',"Color","white")
 grid on; title('x(t)','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$x \,\, [m]$','Interpreter','latex');
 ylim([-0.05 0.25])
+legend("","","","Event",'Interpreter','latex');
 subplot(2,2,2)
-plot(tspan,x(:,2),'LineWidth',2)
+plot(t,x(:,2),'LineWidth',2)
 grid on; title('v(t)','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$v \,\, [m/s^2]$','Interpreter','latex');
 line(xlim, [0,0], 'Color', 'k', 'LineWidth', 0.5);
 hold on
-plot(tspan,x(:,2),'LineWidth',2,'Color',[0, 0.4470, 0.7410])
+plot(t,x(:,2),'LineWidth',2,'Color',[0, 0.4470, 0.7410])
+hold on
+plot(t_event,x_event(2),'MarkerSize',10,'Marker','pentagram','MarkerFaceColor','m','MarkerEdgeColor','k',"Color","white")
 ylim([-0.05 0.4])
+legend("","","","Event",'Interpreter','latex');
 subplot(2,2,3)
-plot(tspan,x(:,3),'LineWidth',2)
+plot(t,x(:,3),'LineWidth',2)
+hold on
+plot(t_event,x_event(3),'MarkerSize',10,'Marker','pentagram','MarkerFaceColor','m','MarkerEdgeColor','k',"Color","white")
 grid on; title('$V_{acc}$(t)','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$V_{acc} \,\, [m^3]$','Interpreter','latex');
 ylim([8.3e-3 8.9e-3])
+legend("","Event",'Interpreter','latex');
 subplot(2,2,4)
-plot(tspan,x(:,4),'LineWidth',2)
+plot(t,x(:,4),'LineWidth',2)
+hold on
+plot(t_event,x_event(4),'MarkerSize',10,'Marker','pentagram','MarkerFaceColor','m','MarkerEdgeColor','k',"Color","white")
 grid on; title('$V_{tank}$(t)','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$V_{tank} \,\, [m^3]$','Interpreter','latex');
 ylim([0.9e-3 1.4e-3])
-
+legend("","Event",'Interpreter','latex');
 figure()
 subplot(1,2,1)
-plot(tspan,rad2deg(param(:,11)),"LineWidth",2)
+plot(t,rad2deg(param(:,11)),"LineWidth",2)
 grid on; title('$\alpha$(t)','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$\alpha \,\, [deg]$','Interpreter','latex');
 ylim([-20 390]);
 subplot(1,2,2)
-plot(tspan,param(:,12),"LineWidth",2)
+plot(t,param(:,12),"LineWidth",2)
 grid on; title('$A_{dist}$(t)','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$A_{dist} \,\, [m^2]$','Interpreter','latex');
 ylim([-0.1e-5 2.1e-5]);
 
 figure()
 subplot(1,2,1)
-plot(tspan,param(:,1),"LineWidth",2)
+plot(t,param(:,1),"LineWidth",2)
 hold on
-plot(tspan,param(:,2),"LineWidth",2,"LineStyle","-.")
+plot(t,param(:,2),"LineWidth",2,"LineStyle","-.")
 hold on
-plot(tspan,param(:,3),"LineWidth",2,"LineStyle",":")
+plot(t,param(:,3),"LineWidth",2,"LineStyle",":")
 hold on
-plot(tspan,param(:,4),"LineWidth",2,"LineStyle","--")
+plot(t,param(:,4),"LineWidth",2,"LineStyle","--")
 grid on
 legend('$P_A$','$P_2$', '$P_3$', '$P_4$','Interpreter','latex')
 title('Pressure behaviour in the upper branch','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$P \,\, [Pa]$','Interpreter','latex');
 subplot(1,2,2)
-plot(tspan,param(:,5),"LineWidth",2)
+plot(t,param(:,5),"LineWidth",2)
 hold on
-plot(tspan,param(:,6),"LineWidth",2,"LineStyle","-.")
+plot(t,param(:,6),"LineWidth",2,"LineStyle","-.")
 hold on
-plot(tspan,param(:,7),"LineWidth",2,"LineStyle",":")
+plot(t,param(:,7),"LineWidth",2,"LineStyle",":")
 hold on
-plot(tspan,param(:,8),"LineWidth",2,"LineStyle","--")
+plot(t,param(:,8),"LineWidth",2,"LineStyle","--")
 grid on
 legend('$P_5$', '$P_6$', '$P_7$', '$P_T$','Interpreter','latex')
 title('Pressure behaviour in the lower branch','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$P \,\, [Pa]$','Interpreter','latex');
 
 figure()
-plot(tspan,param(:,9),"LineWidth",2)
+plot(t,param(:,9),"LineWidth",2)
 hold on
-plot(tspan,param(:,10),"LineWidth",2)
+plot(t,param(:,10),"LineWidth",2)
 grid on
 legend('$Q_1$', '$Q_2$','Interpreter','latex')
 title('Volumetric flow rate','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$Q \,\, [m^3/s]$','Interpreter','latex');
 
 figure()
-plot(tspan,param(:,13),"LineWidth",2)
+plot(t,param(:,13),"LineWidth",2)
 grid on
 title('Actuator force','Interpreter','latex')
 xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$F \,\, [N]$','Interpreter','latex');
@@ -246,16 +257,16 @@ x0 = [Vc_0; 0];     % Initial conditions
 % Case 1: switch closed
 odefun_1 = @(t,x) [x(2);
                    (-x(1)-x(2)*(L/R_1 + C*R_2))/(L*C+L*C*R_2/R_1)];
-[t_1,x_1] = ode45(odefun_1,tspan,x0);
+[t_1,x_1] = ode15s(odefun_1,tspan,x0);
 
 % Case 2: voltage source instead of switch
 v = @(t) sin(2*pi*f*t)*atan(t);
 v_dot = @(t) cos(2*pi*f*t)*(2*pi*f)*atan(t) + sin(2*pi*f*t)/(1+t^2);
 
 odefun_2 = @(t,x) [x(2);...
-                   (-x(1)-x(2)*(L/R_1 + C*R_2) - v(t) - v_dot(t)*L/R_1)/(L*C+L*C*R_2/R_1)];
+                   (-x(1)-x(2)*(L/R_1 + C*R_2) + v(t) + v_dot(t)*L/R_1)/(L*C+L*C*R_2/R_1)];
 
-[t_2,x_2] = ode45(odefun_2,tspan,x0);
+[t_2,x_2] = ode15s(odefun_2,tspan,x0);
 
 % Plots:
 figure()
@@ -274,30 +285,37 @@ xlabel('$Time \,\, [s]$','Interpreter','latex'); ylabel('$V_c \,\, [V]$','Interp
 clearvars; close all; clc;
 
 % Data:
-T_0 = 20;                           %[°C] Initial temperature (all nodes)
-Ti_F = 1000;                        %[°C] Final temperature (internal node)
-To_F = 20;                          %[°C] Final temperature (outer node)
-tspan = 0:60/1e3:60;                %[s] Time vector
-l = [0.01 0.1 0.001 0.05 0.01];     %[m] Layers thickness vector
-k = [390 390 0 7.82 390];           %[W/m/°C] Thermal conductivity
-c = [0 376.12 0 477.29 0];          %[J/kg/°C]  Specific heat
-rho = [8960 8960 8960 7260 8960];   %[kg/m^3] Layers density
-A = 0.1;                            %[m^2] Layers area           
-C = A*rho.*c.*l;                    %[J/°C] Thermal capacitance
+T_0 = 20;                                     %[°C] Initial temperature (all nodes)
+Ti_F = 1000;                                  %[°C] Final temperature (internal node)
+To_F = 20;                                    %[°C] Final temperature (outer node)
+tspan = [0, 60];                              %[s] Time vector
+A = 0.5;                                      %[m^2] Layers area           
+l =     [0.001   0.1  0  0.05  0.001];        %[m] Layers thickness vector
+k =     [   80   413  0    20     80];        %[W/m/°C] Thermal conductivity
+c =     [    0   387  0   470      0];        %[J/kg/°C]  Specific heat
+rho =   [    0  8790  0  4500      0];        %[kg/m^3] Layers density
 
-% Contact resistances computation
+C = A*rho.*c.*l;                              %[J/°C] Thermal capacitance
 R = l./k./A; 
-R(3) = 1/390;
+R(3) = 0.001;
 
 % Internal node temperature profile:
 Ti = @(t) (Ti_F-T_0)*ramp(t,0,1)+T_0;
 
 % Case #1: single node for each layer
-dTdt_1 = @(t,x) [( (Ti(t)-x(1))/(R(1)+R(2)/2)        - (x(1)-x(2))/(R(2)/2+R(4)/2+R(3)) )/C(2);
-                 ( (x(1)-x(2)) /(R(2)/2+R(4)/2+R(3)) - (x(2)-To_F)  /(R(4)/2+R(5))        )/C(4)];
+R_iA = R(1)/2;
+R_AB = R(1)/2 + R(2)/2;
+R_BC = R(2)/2 + R(3)/2;
+R_CD = R(3)/2 + R(4)/2;
+R_DE = R(4)/2 + R(5)/2;
+R_Eo = R(5)/2;
+
+dTdt_1 = @(t,x) [( (Ti(t)-x(1))/(R_iA + R_AB) - (x(1)-x(2))/(R_BC + R_CD))/C(2);
+                 ( (x(1)-x(2)) /(R_BC + R_CD) - (x(2)-To_F)/(R_DE + R_Eo))/C(4)];
+
 % x = [T_B, T_D];       % state vector
 x0_1 = T_0*ones(1,2);   % initial conditions
-[t_1,T_1] = ode45(dTdt_1,tspan,x0_1);
+[t_1,T_1] = ode23s(dTdt_1,tspan,x0_1);
 
 % Nodes array:
 Nodes_1 = zeros(length(t_1),7);
@@ -306,20 +324,29 @@ Nodes_1(:,3) = T_1(:,1);
 Nodes_1(:,5) = T_1(:,2);
 Nodes_1(:,7) = To_F*ones(length(To_F),1);
 for i = 1:length(t_1)
-    Nodes_1(i,2) = Nodes_1(i,1) - R(1)/2            * (Nodes_1(i,1)-Nodes_1(i,3)) / (R(1)+R(2)/2);
-    Nodes_1(i,4) = Nodes_1(i,3) - (R(2)/2 + R(3)/2) * (Nodes_1(i,3)-Nodes_1(i,5)) / (R(3)+R(2)/2+R(4)/2);
-    Nodes_1(i,6) = Nodes_1(i,5) - (R(4)/2 + R(5)/2) * (Nodes_1(i,5)-Nodes_1(i,7)) / (R(4)/2+R(5));
+    Nodes_1(i,2) = Nodes_1(i,1) - R_iA * (Nodes_1(i,1)-Nodes_1(i,3)) / (R_iA + R_AB);
+    Nodes_1(i,4) = Nodes_1(i,3) - R_BC * (Nodes_1(i,3)-Nodes_1(i,5)) / (R_BC + R_CD);
+    Nodes_1(i,6) = Nodes_1(i,5) - R_DE * (Nodes_1(i,5)-Nodes_1(i,7)) / (R_DE + R_Eo);
 end
 
-
 % Case #2: two internal nodes for layers B and D
-dTdt_2 = @(t,x) [( (Ti(t)-x(1))/(R(1)+R(2)/3)        - (x(1)-x(2))/(R(2)/3)             )/C(2)/2;
-                 ( (x(1)-x(2))/(R(2)/3)              - (x(2)-x(3))/(R(2)/3+R(3)+R(4)/3) )/C(2)/2;
-                 ( (x(2)-x(3))/(R(2)/3+R(3)+R(4)/3)  - (x(3)-x(4))/(R(4)/3)             )/C(4)/2;
-                 ( (x(3)-x(4))/(R(4)/3)              - (x(4)-To_F)  /(R(4)/3+R(5))        )/C(4)/2 ];
+R_iA   = R(1)/2;
+R_AB1  = R(1)/2 + R(2)/3;
+R_B1B2 = R(2)/3;
+R_B2C  = R(2)/3 + R(3)/2;  
+R_CD1  = R(3)/2 + R(4)/3;
+R_D1D2 = R(4)/3;
+R_D2E  = R(4)/3 + R(5)/2;
+R_Eo   = R(5)/2;
+
+dTdt_2 = @(t,x) [( (Ti(t)-x(1))/(R_iA + R_AB1)  - (x(1)-x(2))/(R_B1B2)       )/(C(2)/2);
+                 ( (x(1)-x(2)) /(R_B1B2)        - (x(2)-x(3))/(R_B2C + R_CD1))/(C(2)/2);
+                 ( (x(2)-x(3)) /(R_B2C + R_CD1) - (x(3)-x(4))/(R_D1D2)       )/(C(4)/2);
+                 ( (x(3)-x(4)) /(R_D1D2)        - (x(4)-To_F)/(R_D2E + R_Eo) )/(C(4)/2) ];
+
 % x = [T_B1, T_B2, T_D1, T_D2];   % state vector
 x0_2 = T_0*ones(1,4);             % initial conditions
-[t_2,T_2] = ode45(dTdt_2,tspan,x0_2);
+[t_2,T_2] = ode23s(dTdt_2,tspan,x0_2);
 
 % Nodes array:
 Nodes_2 = zeros(length(t_2),9);
@@ -329,16 +356,27 @@ Nodes_2(:,4) = T_2(:,2);
 Nodes_2(:,6) = T_2(:,3);
 Nodes_2(:,7) = T_2(:,4);
 Nodes_2(:,9) = To_F*ones(length(To_F),1);
-
 for i = 1:length(t_2)
-    Nodes_2(i,2) = Nodes_2(i,1) - R(1)/2            * (Nodes_2(i,1)-Nodes_2(i,3)) / (R(1)+R(2)/3);
-    Nodes_2(i,5) = Nodes_2(i,4) - (R(2)/3 + R(3)/2) * (Nodes_2(i,4)-Nodes_2(i,6)) / (R(3)+R(2)/3+R(4)/3);
-    Nodes_2(i,8) = Nodes_2(i,7) - (R(4)/3 + R(5)/2) * (Nodes_2(i,7)-Nodes_2(i,9)) / (R(4)/3+R(5));
+    Nodes_2(i,2) = Nodes_2(i,1) - R_iA  * (Nodes_2(i,1)-Nodes_2(i,3)) / (R_iA + R_AB1);
+    Nodes_2(i,5) = Nodes_2(i,4) - R_B2C * (Nodes_2(i,4)-Nodes_2(i,6)) / (R_B2C + R_CD1);
+    Nodes_2(i,8) = Nodes_2(i,7) - R_D2E * (Nodes_2(i,7)-Nodes_2(i,9)) / (R_D2E + R_Eo);
 end
 
 % Plots:
 figure()
-plot(t_1,Nodes_1,"LineWidth",2);
+plot(t_1,Nodes_1(:,1),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_1,Nodes_1(:,2),"LineWidth",2,"LineStyle","--");
+hold on
+plot(t_1,Nodes_1(:,3),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_1,Nodes_1(:,4),"LineWidth",2,"LineStyle","--");
+hold on
+plot(t_1,Nodes_1(:,5),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_1,Nodes_1(:,6),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_1,Nodes_1(:,7),"LineWidth",2,"LineStyle","--");
 grid on
 legend("$T_i$","$T_A$","$T_B$","$T_C$","$T_D$","$T_E$","$T_0$",'Interpreter','latex')
 title('Node temperature profile','Interpreter','latex')
@@ -346,7 +384,23 @@ xlabel('Time [s]','Interpreter','latex'); ylabel('Temperature [$^\circ$C]','Inte
 ylim([0 1020])
 
 figure()
-plot(t_2,Nodes_2,"LineWidth",2);
+plot(t_2,Nodes_2(:,1),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_2,Nodes_2(:,2),"LineWidth",2,"LineStyle","--");
+hold on
+plot(t_2,Nodes_2(:,3),"LineWidth",2,"LineStyle","-.");
+hold on
+plot(t_2,Nodes_2(:,4),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_2,Nodes_2(:,5),"LineWidth",2,"LineStyle","--");
+hold on
+plot(t_2,Nodes_2(:,6),"LineWidth",2,"LineStyle","-.");
+hold on
+plot(t_2,Nodes_2(:,7),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_2,Nodes_2(:,8),"LineWidth",2,"LineStyle","-");
+hold on
+plot(t_2,Nodes_2(:,9),"LineWidth",2,"LineStyle","--");
 grid on
 legend("$T_1$","$T_A$","$T_{B1}$","$T_{B2}$","$T_C$","$T_{D1}$","$T_{D2}$","$T_E$","$T_0$",'Interpreter','latex')
 title('Node temperature profile','Interpreter','latex')
@@ -369,9 +423,9 @@ function [acc,t,x] = acceleration(x0,Data,k,b)
 % - t:       time vector
 % - x:       result vector
 %
-odefun = @(t,x,k,b) [x(3);...
-                     x(4);...
-                     k*(x(2)-x(1))/Data.J_1;...
+odefun = @(t,x,k,b) [x(3);
+                     x(4);
+                     k*(x(2)-x(1))/Data.J_1;
                      (-k*(x(2)-x(1)) - sign(x(4))*b*(x(4))^2 + Data.T_0)/Data.J_2];
 [t,x] = ode45(@(t,x) odefun(t,x,k,b),Data.tspan,x0);
 
@@ -445,14 +499,14 @@ alpha = 2*acos(1-2*ramp(t,data.t1,data.t2));
 A_dist = data.dist.D^2/8*(alpha-sin(alpha));
 
 % Flow rates
-Q1 = -data.piston.A1*v;
+Q1 = data.piston.A1*v;
 Q2 =  data.piston.A2*v;
 
 % Set a tolerance to avoid dividing by zero
 tol = 1e-10;
 if A_dist > tol
-    v1_dist = (Q1/A_dist)^2;
-    v2_dist = (Q2/A_dist)^2;
+    v1_dist = Q1*abs(Q1)/(A_dist)^2;
+    v2_dist = Q2*abs(Q2)/(A_dist)^2;
 else 
     v1_dist = 0;
     v2_dist = 0;
@@ -460,26 +514,25 @@ end
 
 % Pressures computation
 P_A = data.gas.p0*(data.gas.V0/(data.accum.V-xx(3)))^data.gas.gamma;           
-P_2 = P_A - 0.5*data.fluid.rho*Q1^2*(data.accum.k+data.CV.k)/data.B23.A^2;
-P_3 = P_2 - 0.5*data.fluid.rho*Q1^2*data.B23.f*data.B23.L/data.B23.D/data.B23.A^2;
-P_4 = P_3 - 0.5*data.fluid.rho*data.dist.k*v1_dist;
+P_2 = P_A - 0.5*data.fluid.rho*Q1*abs(Q1)*(data.accum.k+data.CV.k)/data.B23.A^2;
+P_3 = P_2 - 0.5*data.fluid.rho*Q1*abs(Q1)*data.B23.f*data.B23.L/data.B23.D/data.B23.A^2;
 P_T = data.tank.p0;
-P_7 = P_T + 0.5*data.fluid.rho*Q2^2*data.tank.k/data.B67.A^2;
-P_6 = P_7 + 0.5*data.fluid.rho*Q2^2*data.B67.f*data.B67.L/data.B67.D/data.B67.A^2;
+P_7 = P_T + 0.5*data.fluid.rho*Q2*abs(Q2)*data.tank.k/data.B67.A^2;
+P_6 = P_7 + 0.5*data.fluid.rho*Q2*abs(Q2)*data.B67.f*data.B67.L/data.B67.D/data.B67.A^2;
 P_5 = P_6 + 0.5*data.fluid.rho*data.dist.k*v2_dist;
 
-paramout = [P_A, P_2, P_3, P_4, P_5, P_6, P_7, P_T, Q1, Q2, alpha, A_dist, data.piston.F(x)];
+if A_dist >= tol
+    P_4 = P_3 - 0.5*data.fluid.rho*data.dist.k*v1_dist;
+else
+    P_4 = (P_T*data.piston.A2 + data.piston.F(0))/data.piston.A1;
+end
 
 % xdot:
 xdot(1,1) = v;
-
-if A_dist >= tol
-    xdot(2,1) = (P_4*data.piston.A1 - P_5*data.piston.A2 - data.piston.F(x))/data.piston.m;
-else
-    xdot(2,1) = 0;
-end
-xdot(3,1) = Q1;
+xdot(2,1) = (P_4*data.piston.A1 - P_5*data.piston.A2 - data.piston.F(x))/data.piston.m;
+xdot(3,1) = -Q1;
 xdot(4,1) = Q2;
+paramout = [P_A, P_2, P_3, P_4, P_5, P_6, P_7, P_T, Q1, Q2, alpha, A_dist, data.piston.F(x)];
 end
 
 function [value, isterminal, direction] = press_event(~, xx, data)
